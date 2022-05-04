981646f07a340804608152f8a2f0518676de7862
hashwavelab <hashwavelab@gmail.com>
2022-05-04 18:06:07 +0100

    add evn balv2 vaults

---
+++ cfg/evm_balv2_vaults.go
@@ -0,0 +1,28 @@
+package cfg
+
+import (
+	"github.com/hashwavelab/ample/mongo"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+var (
+	EVMBalV2VaultsCollectionName = "evm_balv2_vaults"
+)
+
+type BalV2Vault struct {
+	Name                string   `bson:"name" json:"name"`
+	Source              string   `bson:"source" json:"source"`
+	VaultAddress        string   `bson:"vaultAddress" json:"vaultAddress"`
+	WeightedPoolFactory string   `bson:"weightedPoolFactory" json:"weightedPoolFactory"`
+	WeightedPools       []string `bson:"weightedPools" json:"weightedPools"`
+	StablePoolFactory   string   `bson:"stablePoolFactory" json:"stablePoolFactory"`
+	StablePools         []string `bson:"stablePools" json:"stablePools"`
+	Trading             bool     `bson:"trading" json:"trading"`
+}
+
+func GetAllBalV2VaultsOfOneSource(source string) ([]BalV2Vault, error) {
+	var res []BalV2Vault
+	filter := &bson.M{"source": source}
+	err := mongo.FindAll(EVMBalV2VaultsCollectionName, filter, &res)
+	return res, err
+}
