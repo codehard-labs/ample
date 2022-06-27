ee7e79b99e2fecaecdccbf1e8a611a942d6eda77
codehard-labs <labs@codehard.xyz>
2022-06-27 09:16:32 +0100

    curve: add 2 more fields

--- cfg/evm_curve_pools.go
+++ cfg/evm_curve_pools.go
@@ -10,16 +10,18 @@ var (
 )
 
 type CurvePool struct {
-	Name            string      `bson:"name" json:"name"`
-	Source          string      `bson:"source" json:"source"`
-	Address         string      `bson:"address" json:"address"`
-	LpToken         string      `bson:"lpToken" json:"lpToken"`
-	Type            string      `bson:"type" json:"type"`
-	Coins           []CurveCoin `bson:"coins" json:"coins"`
-	UnderlyingCoins []CurveCoin `bson:"underlyingCoins" json:"underlyingCoins"`
-	BasePool        string      `bson:"basePool" json:"basePool"` // Only applies to meta pools
-	DynamicFee      bool        `bson:"dynamicFee" json:"dynamicFee"`
-	Trading         bool        `bson:"trading" json:"trading"`
+	Name                    string      `bson:"name" json:"name"`
+	Source                  string      `bson:"source" json:"source"`
+	Address                 string      `bson:"address" json:"address"`
+	LpToken                 string      `bson:"lpToken" json:"lpToken"`
+	Type                    string      `bson:"type" json:"type"`
+	Coins                   []CurveCoin `bson:"coins" json:"coins"`
+	UnderlyingCoins         []CurveCoin `bson:"underlyingCoins" json:"underlyingCoins"`
+	BasePool                string      `bson:"basePool" json:"basePool"` // Only applies to meta pools
+	DynamicFee              bool        `bson:"dynamicFee" json:"dynamicFee"`
+	HasReceiver             bool        `bson:"hasReceiver" json:"hasReceiver"`
+	ExchangeUnderlyingProxy string      `bson:"exchangeUnderlyingProxy" json:"exchangeUnderlyingProxy"`
+	Trading                 bool        `bson:"trading" json:"trading"`
 }
 
 type CurveCoin struct {
